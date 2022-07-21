321f3d8eb7822490714d8484bca03e4e2ddc4c03
Русанова Анна <rusanova.annette@gmail.com>
2022-07-21 19:32:13 +0100

    f

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -106,7 +106,7 @@ class UserViewSet(viewsets.ModelViewSet):
             serializer = MeSerializer(user, data=request.data, partial=True)
             serializer.is_valid(raise_exception=True)
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
